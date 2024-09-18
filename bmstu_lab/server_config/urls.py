a13475dda4958c33e47f473d10c82e149802a798
DESKTOP-VAAI8CE\GALIM <galim04.ga@gmail.com>
2024-09-18 11:07:30 +0300

    Initial Django project setup

---
+++ bmstu_lab/server_config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from lab01 import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.components, name='components'),
+    path('search/', views.components, name='search'),
+    path('component/<int:id>/', views.component, name='component'),
+    path('request/<int:id>/', views.request, name='request')
+]
