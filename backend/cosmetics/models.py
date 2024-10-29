b290fce2b287fc79db7b2de423dad18aee1cff8d
DESKTOP-VAAI8CE\GALIM <galim04.ga@gmail.com>
2024-10-29 14:51:40 +0300

    Remove cascade deletion and add field 'name' to cosmetic

--- backend/cosmetics/models.py
+++ backend/cosmetics/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 # Модель для химических элементов
@@ -36,29 +35,23 @@ class CosmeticOrder(models.Model):
         (5, 'Отклонено'),
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE,
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             null=True, blank=True,
                              verbose_name="Пользователь", related_name="created_formulation")
     status = models.IntegerField(
         choices=STATUS_CHOICES, default=1, verbose_name="Статус")
     date_created = models.DateTimeField(
         auto_now_add=True, verbose_name="Дата создания")
-    category = models.CharField(
-        max_length=50, verbose_name="Категория косметики", blank=True, null=True)
-    manager = models.ForeignKey(User, on_delete=models.CASCADE,
-                                verbose_name="Менеджер", related_name="managed_formulation", blank=True, null=True)
+    name = models.CharField(
+        max_length=50, verbose_name="Название косметики", blank=True, null=True)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                null=True, blank=True,
+                                verbose_name="Менеджер", related_name="managed_formulation")
     date_formation = models.DateTimeField(blank=True, null=True)
     date_completion = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return f"Заказ №{self.pk} от {self.user.username}"
-
-    def save(self, *args, **kwargs):
-        # Проверка на наличие уже существующей заявки в статусе "черновик"
-        if self.status == 1:  # 1 - это статус "Черновик"
-            if CosmeticOrder.objects.filter(user=self.user, status=1).exists():
-                raise ValidationError(
-                    "У вас уже есть заявка в статусе 'Черновик'.")
-        super().save(*args, **kwargs)
+        return f"Заказ №{self.pk} от {self.user.username if self.user else 'Удалённый пользователь'}"
 
     class Meta:
         verbose_name = "Косметическая заявка"
@@ -69,13 +62,13 @@ class CosmeticOrder(models.Model):
 # Модель для компонентов в заявке
 class OrderComponent(models.Model):
     order = models.ForeignKey(
-        CosmeticOrder, on_delete=models.CASCADE, related_name="components")
+        CosmeticOrder, on_delete=models.SET_NULL, null=True, blank=True, related_name="components")
     chemical_element = models.ForeignKey(
-        ChemicalElement, on_delete=models.CASCADE, verbose_name="Химический элемент")
-    dosage = models.FloatField(verbose_name="Дозировка")
+        ChemicalElement, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Химический элемент")
+    dosage = models.FloatField(verbose_name="Дозировка", default=0)
 
     def __str__(self):
-        return f"{self.chemical_element.title} в заявке №{self.order.pk}"
+        return f"{self.chemical_element.title if self.chemical_element else 'Удалённый элемент'} в заявке №{self.order.pk if self.order else 'Удалённая заявка'}"
 
     class Meta:
         verbose_name = "Компонент заявки"
